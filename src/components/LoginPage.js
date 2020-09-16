9292df5d8335eb9fb00446ee032c6e49fed48cf9
Ylva Norrby <ylva.norrby@cygni.se>
2020-09-16 09:02:22 +0200

    Add private routes

---
+++ src/components/LoginPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { startLogin } from '../actions/auth';
+
+export const LoginPage = ({ startLogin }) => (
+    <div>
+        <button onClick={startLogin}>Log in</button>
+    </div>
+);
+
+const mapDispathToProps = (dispatch) => ({
+    startLogin: () => dispatch(startLogin())
+});
+
+export default connect(undefined, mapDispathToProps)(LoginPage);
